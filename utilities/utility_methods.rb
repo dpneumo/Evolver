2f8d3f97e47e13405d7719548b25e85c999b3c1a
DocPneumo <dpneumo@gmail.com>
2021-10-31 00:47:15 -0500

    mean should return a Float

--- utilities/utility_methods.rb
+++ utilities/utility_methods.rb
@@ -2,7 +2,7 @@
 
 module UtilityMethods
   def roll(loaded_die)
-    loaded_die.max_by { |_, weight| rand**(1.0 / weight) }.first
+    loaded_die.max_by {|_, weight| rand**(1.0 / weight) }.first
   end
 
   def flip(biased_coin)
@@ -10,6 +10,6 @@ module UtilityMethods
   end
 
   def mean(total, weighted_value)
-    total.zero? ? 0 : weighted_value.fdiv(total)
+    total.zero? ? 0.0 : weighted_value.fdiv(total)
   end
 end
