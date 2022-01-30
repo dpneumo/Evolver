5a28d5f207131200713c59ec9597f5da9b4ce7fa
DocPneumo <dpneumo@gmail.com>
2022-01-29 19:58:47 -0600

    Refactor Publisher

--- actors/publisher.rb
+++ actors/publisher.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Publisher
   include UtilityMethods
 
@@ -8,11 +6,13 @@ class Publisher
   end
 
   def publish
-    puts "pop stats by period:\n"
-    pop_stats_by_period
-    puts "\n"
-    puts "mean age at death by_species:\n"
-    mean_age_at_death_all
+    puts <<~TEXT
+      pop stats by period:
+      #{pop_stats_by_period}
+      mean age at death by_species:
+      #{mean_age_at_death_all}
+    TEXT
+    nil
   end
 
   private
@@ -21,24 +21,22 @@ class Publisher
     end
 
     def pop_stats_by_period
-      @stats.pop_counts.each do |period, species_data|
-        species_data.each do |species, sums|
-          count = sums[:summed_count]; summed_ages = sums[:summed_ages]
-          puts "period: #{period}, species: #{species}, population: #{count}, mean age: #{mean_age(count, summed_ages)}"
-        end
-        puts "\n"
+      @stats.pop_counts.each.inject('') do |text, (period, species_data)|
+        text << "  period: #{period}\n"
+        text << species_data.each.inject('') do |txt, (species, sums)|
+                  count = sums[:summed_count]; summed_ages = sums[:summed_ages]
+                  txt << "    species: #{species}, population: #{count}, mean age: #{mean_age(count, summed_ages)}\n"
+                  txt
+                end
+        text
       end
     end
 
     def mean_age_at_death_all
-       @stats.dac_all.each do |species, sums|
+      @stats.dac_all.each.inject('') do |txt, (species, sums)|
         count = sums[:summed_count]; summed_ages = sums[:summed_ages]
-        puts "species: #{species}, mean_age_at_death: #{mean_age(count, summed_ages);}"
+        txt << "  species: #{species}, mean_age_at_death: #{mean_age(count, summed_ages)}\n"
+        txt
       end
-    end
-
-    def mean_age_at_death(color_id)
-      stats = @stats.death_age_counts(color_id: color_id)
-      mean(count: stats[:count], sum: stats[:summed_ages]).round(2)
     end
 end
--- tests/actors/publisher_test.rb
+++ tests/actors/publisher_test.rb
@@ -5,15 +5,19 @@ require_relative '../../actors/publisher'
 
 class PublisherTest < Minitest::Test
   def test_publishes_a_stats_summary
-  	toolbox = MockToolbox.new
-  	@publisher = Publisher.new(toolbox: toolbox)
-  	expected = "pop stats by period:
-period: 1, species: coyote, population: 2, mean age: 1.0
-period: 2, species: coyote, population: 3, mean age: 1.33
+  	@publisher = Publisher.new(toolbox: MockToolbox.new)
+  	expected = <<~EXPECTED
+      pop stats by period:
+        period: 1
+          species: coyote, population: 2, mean age: 1.0
+        period: 2
+          species: coyote, population: 3, mean age: 1.33
 
-mean age at death by_species:
-species: coyote, mean_age_at_death: 4.67
-"
+      mean age at death by_species:
+        species: coyote, mean_age_at_death: 4.67
+
+    EXPECTED
+
   	assert_output(expected) {@publisher.publish}
   end
 end
